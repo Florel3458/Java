e6770155ec399f0ee3741af4719c41bf337ee9f6
Florel3458 <florelyang@gmail.com>
2021-11-01 17:23:38 -0400

    2021-Nov-1 inClass update

---
+++ Ch1/TablesAndChairs.java
@@ -0,0 +1,10 @@
+public class TablesAndChairs {
+    public static void main(String[] args) {
+        System.out.println("X                    X");
+        System.out.println("X                    X");
+        System.out.println("X     XXXXXXXXXX     X");
+        System.out.println("Xxxxx X        X xxxxX");
+        System.out.println("X   X X        X X   X");
+        System.out.println("X   X X        X X   X");
+    }
+}
