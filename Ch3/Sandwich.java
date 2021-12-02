6f4d71770986cace91b36acb454b44ff543ae704
Florel3458 <florelyang@gmail.com>
2021-12-01 21:00:43 -0500

    changed folders, more inclass

---
+++ Ch3/Sandwich.java
@@ -0,0 +1,26 @@
+public class Sandwich {
+    private String mainIngredient = "tuna";
+    private String breadType = "wheat";
+    private double price = 4.99;
+
+    public String getMainIngredient() {
+        return mainIngredient;
+    }
+    public void setMainIngredient(String ing) {
+        mainIngredient = ing;
+    }
+
+    public String getbreadType() {
+        return breadType;
+    }
+    public void setBreadType(String brd) {
+        breadType = brd;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double pr) {
+        price = pr;
+    }
+}
---
+++ Ch3/TestSandwich.java
@@ -0,0 +1,5 @@
+public class TestSandwich {
+    public static void main(String[] args) {
+        Sandwich getSandwich();
+    }
+}
