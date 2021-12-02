3f557677f81ddef85d7b3492f45305ea16b0e776
Florel3458 <florelyang@gmail.com>
2021-12-01 21:30:41 -0500

    the sandwich works!

--- Ch3/TestSandwich.java
+++ Ch3/TestSandwich.java
@@ -1,5 +1,15 @@
 public class TestSandwich {
     public static void main(String[] args) {
-        Sandwich getSandwich();
+        Sandwich testSandwich = new Sandwich();
+
+        System.out.println("A sandwich with " + testSandwich.getMainIngredient() + " and\n" + 
+        testSandwich.getbreadType() + " bread will cost $" + testSandwich.getPrice() + ".");
+
+        testSandwich.setMainIngredient("salami");
+        testSandwich.setBreadType("white");
+        testSandwich.setPrice(3.14);
+
+        System.out.println("\nBut a sandwich with " + testSandwich.getMainIngredient() + " and\n" + 
+        testSandwich.getbreadType() + " bread will cost $" + testSandwich.getPrice() + ".");
     }
 }
